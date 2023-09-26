cc07c25bd7fcf213195f22bd3addaa8fa14b809d
CodeCraftAcademy981 <144753775+CodeCraftAcademy981@users.noreply.github.com>
2023-09-25 20:19:52 -0700

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,5 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry) =>{
         console.log(entry)
