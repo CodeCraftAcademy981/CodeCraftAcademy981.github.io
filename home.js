f7f392d8165b1ec0cc39eb58e9a1d49e3fbb780e
CodeCraftAcademy981 <144753775+CodeCraftAcademy981@users.noreply.github.com>
2023-10-02 16:02:10 -0700

    Update home.js

--- home.js
+++ home.js
@@ -1,5 +1,9 @@
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry) =>{
         console.log(entry)
