bf8e7ec0b3bc5d04512f46c4126a7df8060465a8
CodeCraftAcademy981 <144753775+CodeCraftAcademy981@users.noreply.github.com>
2023-10-15 20:07:50 -0700

    Update form.js

--- form.js
+++ form.js
@@ -1,3 +1,5 @@
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 const signInBtn = document.getElementById("signIn");
 const signUpBtn = document.getElementById("signUp");
